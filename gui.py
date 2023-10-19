f7a8f4f9f44abefedeed39c269300b08d63978b7
Marida Um <maridaum7@yahoo.com>
2023-10-19 13:43:48 -0700

    Implement edit, complete, and exit button functionality

--- gui.py
+++ gui.py
@@ -4,22 +4,51 @@ import PySimpleGUI as sg
 label = sg.Text("Type in a to-do")
 input_box = sg.InputText(tooltip="Enter todo", key="todo")
 add_btn = sg.Button("Add")
+list_box = sg.Listbox(values=functions.get_todos(), key="todos",
+                      enable_events=True, size=[45, 10])
+edit_btn = sg.Button("Edit")
+complete_btn = sg.Button("Complete")
+exit_btn = sg.Button("Exit")
+
+layout = [[label, input_box, add_btn],
+          [list_box, edit_btn, complete_btn],
+          [exit_btn]]
 
 window = sg.Window("My To-Do App",
-                   layout=[[label, input_box, add_btn]],
+                   layout=layout,
                    font=("Helvetica", 20))
 
 while True:
     event, values = window.read()
     print(event)
     print(values)
+    print(values["todos"])
     match event:
         case "Add":
             todos = functions.get_todos()
-            new_todo = values['todo'] + "\n"
+            new_todo = values["todo"] + "\n"
             todos.append(new_todo)
             functions.write_todos(todos)
-        case sg.WIN_CLOSED:
+            window["todos"].update(values=todos)
+        case "Edit":
+            todo_to_edit = values["todos"][0]
+            new_todo = values['todo'] + "\n"
+
+            todos = functions.get_todos()
+            index = todos.index(todo_to_edit)
+            todos[index] = new_todo
+            functions.write_todos(todos)
+            window["todos"].update(values=todos)
+        case "Complete":
+            todo_to_complete = values["todos"][0]
+            todos = functions.get_todos()
+            todos.remove(todo_to_complete)
+            functions.write_todos(todos)
+            window["todos"].update(values=todos)
+            window["todo"].update(value="")
+        case "todos":
+            window["todo"].update(value=values["todos"][0])
+        case sg.WIN_CLOSED | "Exit":
             break
 
 window.close()
